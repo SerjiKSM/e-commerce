dda949b9adf0e56a4d68c3de3b5271a8302168d9
SK <sergiykrohmalniy@gmail.com>
2024-06-26 08:43:11 +0300

    add notification service

---
+++ services/notification/src/main/java/com/ksm/notification/enumeration/NotificationType.java
@@ -0,0 +1,6 @@
+package com.ksm.notification.enumeration;
+
+public enum NotificationType {
+    ORDER_CONFIRMATION,
+    PAYMENT_CONFIRMATION
+}
---
+++ services/notification/src/main/java/com/ksm/notification/record/Product.java
@@ -0,0 +1,12 @@
+package com.ksm.notification.record;
+
+import java.math.BigDecimal;
+
+public record Product(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
